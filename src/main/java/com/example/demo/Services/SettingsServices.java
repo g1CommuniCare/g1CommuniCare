eed8737fea9597ac7ce977ca6b807dafee1806bf
Gieb Mier <128208941+giebm@users.noreply.github.com>
2023-11-19 20:09:34 +0800

    Entity, Repository, and Services for Settings

---
+++ src/main/java/com/example/demo/Services/SettingsServices.java
@@ -0,0 +1,15 @@
+package com.example.demo.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.SettingsEntity;
+import com.example.demo.Repository.SettingsRepository;
+
+@Service
+public class SettingsServices {
+
+    @Autowired
+    SettingsRepository srepo;
+
+}
