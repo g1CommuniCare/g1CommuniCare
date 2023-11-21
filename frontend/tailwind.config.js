b44aa66bf8b21aa6457967e886f78121fe937674
showah <joshuabborres@gmail.com>
2023-11-21 09:57:09 +0800

    addition and application of fonts and minor layout changes in the resident landing page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,12 +2,28 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
         extend: {
-            fontFamily: {
-                sans: ['Poppins', 'sans-serif'],
-            },
+          fontFamily: {
+            'sans': ['Roboto', 'sans-serif'],
+            'librefranklin': ['Libre Franklin', 'sans-serif'],
+            'robotoserif': ['Roboto Serif', 'serif'],
+            'archivo': ['Archivo', 'sans-serif'],
+            'marcellus': ['Marcellus', 'serif'],
+            'molengo': ['Molengo', 'serif'],
+          },
+          fontWeight: {
+            'thin': 100,
+            'extralight': 200,
+            'light': 300,
+            'normal': 400,
+            'medium': 500,
+            'semibold': 600,
+            'bold': 700,
+            'extrabold': 800,
+            'black': 900,
+          },
         },
-    },
-    plugins: [],
+      },
+      variants: {},
+      plugins: [],
 };
