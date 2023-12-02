27c362ef766468ea670f75707735385bba773eee
Bermar Villarazo Jr <villarazobermar@gmail.com>
2023-12-02 18:39:31 +0800

    Deleted page.jsx

--- frontend/app/(admin)/admin-document-request/[docreqId]/page.jsx
+++
@@ -1,6 +0,0 @@
-
-// // const DocumentRequestDetail = ({docreqId}) => {
-// //   const [requestData, setRequestData] = useState(null);
-// //     console.log("requestData: ", requestData);
-
-// //     async function fetchData() {
